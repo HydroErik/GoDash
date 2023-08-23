9ac278a1557f0766917a129a184e45538a893019
HydroErik <esunblad@hydrologik.com>
2023-08-23 11:55:59 -0600

    Agent view back end complete : Begining of Auth

--- mongDrive/mongDrive.go
+++ mongDrive/mongDrive.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	//"os"
+	//"reflect"
 	"time"
 
+	//"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -23,11 +27,18 @@ type Report struct {
 }
 
 type Server struct {
-	Name	string
-	Cpu		[]int16
-	Drives	[]map[string]float32
+	Name      string
+	Cpu       []float64
+	Drives    map[string][]float64
+	TimeStamp []time.Time
 }
 
+type User struct {
+	Email    string
+	Username string
+	Password string
+	Name     string
+}
 
 func EncryptPass(old_pass string) (string, error) {
 	//encoded := base64.StdEncoding.EncodeToString([]byte(old_pass))
@@ -48,15 +59,25 @@ func GetDBNames(client *mongo.Client, db_names chan []string) {
 }
 
 // Get the list of site returns for a given agent DB
+// When report name passsed in return single record
 // Returns list of maps with for given agent with common structure
-func GetAgentReports(client *mongo.Client, db string, reports chan []Report) {
+func GetAgentReports(client *mongo.Client, db string, reportName string, reports chan map[string]Report) {
 
 	reportCol := client.Database(db).Collection("Report")
-	cur, err := reportCol.Find(context.TODO(), bson.D{}) //Get all the reports
+
+	//Need to pre-declare to avoid nill operator
+	var cur *mongo.Cursor
+	var err error
+
+	if reportName != "" {
+		cur, err = reportCol.Find(context.TODO(), bson.D{{"name", reportName}}) //Get a single report
+	} else {
+		cur, err = reportCol.Find(context.TODO(), bson.D{}) //Get all report
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
-	var repLst []Report
+	repDic := make(map[string]Report)
 
 	for cur.Next(context.TODO()) {
 		var reportRaw bson.M
@@ -64,6 +85,7 @@ func GetAgentReports(client *mongo.Client, db string, reports chan []Report) {
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		var newReport = Report{
 			Name:        reportRaw["name"].(string),
 			Err_flag:    reportRaw["Error Flag"].(bool),
@@ -73,13 +95,127 @@ func GetAgentReports(client *mongo.Client, db string, reports chan []Report) {
 			Source:      reportRaw["source"].(string),
 			ErrStr:      reportRaw["error"].(string),
 		}
-		repLst = append(repLst, newReport)
+		repDic[reportRaw["name"].(string)] = newReport
 	}
-
-	reports <- repLst
+	reports <- repDic
 
 }
 
+// Get the list of reports for a given agent database
+// Takes a client the db_name, and a chanel for the return []string
+func GetAgentReportList(client *mongo.Client, db string, reportNames chan map[string]bool) {
+	reportCol := client.Database(db).Collection("Report")
+	opts := options.Find().SetProjection(bson.D{{"name", 1}, {"Error Flag", 1}})
+	cur, err := reportCol.Find(context.TODO(), bson.D{}, opts) //Get all the reports
+	if err != nil {
+		log.Fatal(err)
+	}
+	var ret = make(map[string]bool)
+	for cur.Next(context.TODO()) {
+
+		var value bson.M
+		err := cur.Decode(&value)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ret[value["name"].(string)] = value["Error Flag"].(bool)
+	}
+	reportNames <- ret
+}
+
 // Return Server Data about all current hydrologik servers
-// Returns a list of structs about each given server 
-func GetServerReports(client *mongo.Client, servers chan []Server)
+// Returns a list of structs about each given server
+func GetServerReports(client *mongo.Client, servers chan []Server) {
+	var srvLst []Server
+	srvDb := client.Database("agent_server")
+
+	colLst, err := srvDb.ListCollectionNames(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, col := range colLst {
+		if col == "log" {
+			continue
+		}
+		srv := srvDb.Collection(col)
+		cur, err := srv.Find(context.TODO(), bson.D{})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var cpu []float64
+		drives := make(map[string][]float64)
+		var timeStamps []time.Time
+
+		for cur.Next(context.TODO()) {
+			var reportRaw bson.M
+			err := cur.Decode(&reportRaw)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			cpu = append(cpu, reportRaw["cpu_perc"].(float64))
+			timeStamps = append(timeStamps, reportRaw["_id"].(primitive.DateTime).Time())
+			//keys := reflect.ValueOf(reportRaw["drives"].(primitive.M)).MapKeys()
+
+			for i, dic := range reportRaw["drives"].(primitive.M) {
+
+				val := dic.(float64)
+				drives[i] = append(drives[i], val)
+			}
+		}
+		var newSrv = Server{Name: col, Cpu: cpu, Drives: drives, TimeStamp: timeStamps}
+		srvLst = append(srvLst, newSrv)
+	}
+
+	servers <- srvLst
+}
+
+// Return dict of authenticated user from db
+func GetAuths(client *mongo.Client) map[string]User {
+	reportCol := client.Database("GoUsers").Collection("gocredentials")
+	cur, err := reportCol.Find(context.TODO(), bson.D{})
+	if err != nil {log.Fatal(err)}
+
+	var userMap = make(map[string]User)
+
+	for cur.Next(context.TODO()) {
+		dbUser := bson.M{}
+		err := cur.Decode(&dbUser)
+		if err != nil{log.Fatal(err)}
+		newUser := User{
+			Email: dbUser["email"].(string),
+			Username: dbUser["username"].(string),
+			Password: dbUser["password"].(string),
+			Name: dbUser["name"].(string),
+		}
+		userMap[newUser.Username] = newUser
+	}
+	return userMap
+
+}
+
+/*
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found")
+	}
+
+	//var agnetChan = make(chan []string, 0)
+	var agentRep = make(chan map[string]Report)
+	mongo_uri := os.Getenv("MONGOSTRING")
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongo_uri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//go GetAgentReportList(client, "agent_Go4", agnetChan)
+	//var val = agnetChan
+	//fmt.Println(<-val)
+
+	go GetAgentReports(client, "agent_Go4", "JOPOUTCO:STAGE", agentRep)
+	var val = agentRep
+	fmt.Println(<-val)
+}
+*/
--- src/server.go
+++ src/server.go
@@ -7,54 +7,136 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"math/rand"
 
 	"hydrodash/mongDrive"
 
+	"github.com/gorilla/sessions"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var templates = template.Must(template.ParseFiles("../Templates/index.html", "../Templates/login.html"))
+var u = uint8(rand.Intn(255))
+
+var (
+	
+	key = []byte{239,  57, 183,  33, 121, 175, 214,  u,  52, 235,  33, 167,  74,  91, 153,  39 }
+	store = sessions.NewCookieStore(key)
+)
+
+var authDict map[string]mongDrive.User
+
+var templates = template.Must(template.ParseFiles(
+	"../Templates/index.html",
+	"../Templates/login.html",
+	"../Templates/agents.html",
+	"../Templates/reports.html",
+))
 
 // a authenticated c cookie, u username, e email
 type Session struct {
-	A bool
-	C []byte
-	U string
-	E string
+	Auth     bool
+	Cookie   []byte
+	Username string
+	Email    string
 }
 
-var curSession = Session{A: false}
+type Agent struct {
+	Name        string
+	Mongo       *mongo.Client
+	ReportFunc  func(*mongo.Client, string, string, chan map[string]mongDrive.Report)
+	NamesFunc   func(*mongo.Client, string, chan map[string]bool)
+	ReportErr   chan map[string]bool
+	Reports     chan map[string]mongDrive.Report
+}
 
-func renderTemplate(w http.ResponseWriter, tmpl string) {
+func (a Agent) reportList() {
+	go a.NamesFunc(a.Mongo, a.Name, a.ReportErr)
+}
 
-	err := templates.ExecuteTemplate(w, tmpl+".html", "")
+func (a Agent) singleReport(reportName string) {
+	go a.ReportFunc(a.Mongo, a.Name, reportName, a.Reports)
+}
+
+var agents = make(map[string]Agent)
+var agentList []string
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data any) {
+	//fmt.Println(data)
+	err := templates.ExecuteTemplate(w, tmpl+".html", data)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
+// Handler Functions///////////////////////////////////////////////////////////////////////
 func indexHanlder(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("index handler called")
-	renderTemplate(w, "index")
+	//fmt.Println("index handler called")
+	renderTemplate(w, "index", agentList)
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Login handler Called")
-	renderTemplate(w, "login")
+	//fmt.Println("Login handler Called")
+	renderTemplate(w, "login", "")
 }
 
 func validateHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Validate Handler Called")
-	curSession.A = true
+	//fmt.Println("Validate Handler Called")
+	session, _ := store.Get(r, "cookie-name")
+	_ = r.ParseForm()
+	fmt.Println(r.Form["Username"])
+	fmt.Println(r.Body)
+	fmt.Println(r.PostForm)
+    session.Values["authenticated"] = true
+    session.Save(r, w)
 	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+func agentHandler(w http.ResponseWriter, r *http.Request, agentName string) {
+	a := strings.Split(agentName, "/")[2]
+	agent := agents[a]
+	agent.reportList()
+	orgNames := map[string][]string{"Error": nil, "Working": nil}
+	
+	//Parse reports into lists of working or not
+	for key, elm := range <- agent.ReportErr{
+		if elm{
+			orgNames["Error"] = append(orgNames["Error"], key)
+		}else{
+			orgNames["Working"] = append(orgNames["Working"], key)
+		}
+	}
+
+
+	data := struct {
+		Db      string
+		Error []string
+		Working []string
+	}{
+		Db:      a,
+		Error: orgNames["Error"],
+		Working: orgNames["Working"],
+	}
+	renderTemplate(w, "agents", data)
+}
+
+func reportHandler(w http.ResponseWriter, r *http.Request, path []string) {
+	agentName := path[2]
+	reportName := path[3]
+	agents[agentName].singleReport(reportName)
+	var report = <-agents[agentName].Reports
+	renderTemplate(w, "reports", report[reportName])
+}
+
+// Handler Makers//////////////////////////////////////////////////////////////////////////////
 func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if !curSession.A {
+		session, _ := store.Get(r, "cookie-name")
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 			fmt.Println("Redirecting per auth")
 			http.Redirect(w, r, "/login/", http.StatusFound)
 		}
@@ -62,39 +144,83 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	}
 }
 
+func makeReportHandler(fn func(http.ResponseWriter, *http.Request, []string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "cookie-name")
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			fmt.Println("Redirecting per auth")
+			http.Redirect(w, r, "/login/", http.StatusFound)
+		}
+		path := string(r.URL.Path)
+		pathArr := strings.Split(path, "/")
+		if pathArr[2] == "" {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, pathArr)
+	}
+}
+
+func makeAgentHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "cookie-name")
+
+		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
+			fmt.Println("Redirecting per auth")
+			http.Redirect(w, r, "/login/", http.StatusFound)
+		}
+		agent := string(r.URL.Path)
+		if agent == "" {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, agent)
+	}
+}
+
+// Main//////////////////////////////////////////////////////////////////////////////////////////////////////
 func main() {
 	//Use .env for consection string
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
 
-	var db_chan = make(chan []string, 0)
-	var rep_chan = make(chan []mongDrive.Report, 0)
+	var db_chan = make(chan []string)
+	//var rep_chan = make(chan map[string]mongDrive.Report, 0)
+	var srv_chan = make(chan []mongDrive.Server)
+	//var repName_chan = make(chan []string, 0)
 	mongo_uri := os.Getenv("MONGOSTRING")
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(mongo_uri))
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	authDict = mongDrive.GetAuths(client)
+
+
 	go mongDrive.GetDBNames(client, db_chan)
 	dbList := db_chan
 
+	//Get Agents and report should extract to function to update every 900,000 ms
 	for _, db := range <-dbList {
-		fmt.Println(db)
-
+		split := strings.Split(db, "_")
+		if len(split) > 1 && split[1] != "server" {
+			agentList = append(agentList, db)
+			//Each agent gets its own set of chanels and goroutines that we start here.
+			newAgent := Agent{Name: db, Mongo: client, ReportFunc: mongDrive.GetAgentReports, NamesFunc: mongDrive.GetAgentReportList,  Reports: make(chan map[string]mongDrive.Report), ReportErr: make(chan map[string]bool)}
+			agents[db] = newAgent
+		}
 	}
 
-	go mongDrive.GetAgentReports(client, "agent_Go4", rep_chan)
-	repLst := rep_chan
+	go mongDrive.GetServerReports(client, srv_chan)
+	//srvLst := srv_chan
 
-	for _, rep := range <-repLst {
-		fmt.Println(rep)
+	http.HandleFunc("/", makeHandler(indexHanlder))
+	http.HandleFunc("/login/", loginHandler)
+	http.HandleFunc("/validate/", validateHandler)
+	http.HandleFunc("/agents/", makeAgentHandler(agentHandler))
+	http.HandleFunc("/reports/", makeReportHandler(reportHandler))
 
-	}
-
-	//http.HandleFunc("/", makeHandler(indexHanlder))
-	//http.HandleFunc("/login/", loginHandler)
-	//http.HandleFunc("/validate/", validateHandler)
-
-	//log.Fatal(http.ListenAndServe(":8809", nil))
+	log.Fatal(http.ListenAndServe(":8809", nil))
 }
